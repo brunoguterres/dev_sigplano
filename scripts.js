94b2e1e6544f03fe802a2ee98b2a1c041ebe7f76
brunoguterres <brunoguterres26@gmail.com>
2024-07-08 17:27:55 -0300

    atualização

--- scripts.js
+++ scripts.js
@@ -1,18 +1,17 @@
-var mapView = new ol.View ({
-    center: ol.proj.fromLonLat([-36.3, -7.6]),
-    zoom: 9,
+// Definir a visualização inicial do mapa
+var mapView = new ol.View({
+    center: ol.proj.fromLonLat([-37, -7.6]),
+    zoom: 8,
 });
 
+// Criar o mapa
 var map = new ol.Map({
     target: 'map',
     view: mapView,
 });
 
-var osmTile = new ol.layer.Tile ({
-    title: 'Open Street Map',
-    visible: true,
-    opacity: 0.8,
-    type: 'base',
+// Adicionar camada OpenStreetMap como base
+var osmTile = new ol.layer.Tile({
     source: new ol.source.OSM(),
 });
 
