e206ea1ad73cd3d1fb537dc1baf6e97aeae6768f
brunoguterres <brunoguterres26@gmail.com>
2024-07-15 11:21:03 -0300

    atualização

--- camadas.js
+++ camadas.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const chkBaciaRioParaiba = document.getElementById('checkboxBaciaRioParaiba');
     const chkSubBaciaRioParaiba = document.getElementById('checkboxSubBaciaRioParaiba');
     const chkHidrografiaPrincipal = document.getElementById('checkboxHidrografiaPrincipal');
-    
+    const chkAcudes = document.getElementById('checkboxAcudes');
+    const chkAcudesMonitorados = document.getElementById('checkboxAcudesMonitorados');
+    const chkPontosAcudesMonitorados = document.getElementById('checkboxPontosAcudesMonitorados');
+    const chkPontosAcudesEstrategicos = document.getElementById('checkboxPontosAcudesEstrategicos');
+
     chkBaciaRioParaiba.addEventListener('change', function() {
         baciaRioParaiba.setVisible(this.checked);
     });
@@ -12,6 +16,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     chkHidrografiaPrincipal.addEventListener('change', function() {
         hidrografiaPrincipal.setVisible(this.checked);
+    });
+    chkAcudes.addEventListener('change', function() {
+        acudes.setVisible(this.checked);
+    });
+    chkAcudesMonitorados.addEventListener('change', function() {
+        acudesMonitorados.setVisible(this.checked);
+    });
+    chkPontosAcudesMonitorados.addEventListener('change', function() {
+        pontosAcudesMonitorados.setVisible(this.checked);
+    });
+    chkPontosAcudesEstrategicos.addEventListener('change', function() {
+        pontosAcudesEstrategicos.setVisible(this.checked);
     });
 });
 
@@ -52,9 +68,57 @@ var hidrografiaPrincipal = new ol.layer.Tile({
     visible: true,
 })
 
+var acudes = new ol.layer.Tile({
+    title: 'Açudes',
+    legendUrl: url_geoserver+'?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=prh_rpb:acudes_rpb',
+    source: new ol.source.TileWMS({
+        url: url_geoserver,
+        params: {'LAYERS':'prh_rpb:acudes_rpb', 'TILED':true},
+        serverType: 'geoserver',
+    }),
+    visible: true,
+})
+
+var acudesMonitorados = new ol.layer.Tile({
+    title: 'Açudes Monitorados',
+    legendUrl: url_geoserver+'?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=prh_rpb:acudes_monitorados_rpb',
+    source: new ol.source.TileWMS({
+        url: url_geoserver,
+        params: {'LAYERS':'prh_rpb:acudes_monitorados_rpb', 'TILED':true},
+        serverType: 'geoserver',
+    }),
+    visible: true,
+})
+
+var pontosAcudesMonitorados = new ol.layer.Tile({
+    title: 'Pontos Açudes Monitorados',
+    legendUrl: url_geoserver+'?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=prh_rpb:loc_acudes_monitorados_rpb',
+    source: new ol.source.TileWMS({
+        url: url_geoserver,
+        params: {'LAYERS':'loc_acudes_monitorados_rpb', 'TILED':true},
+        serverType: 'geoserver',
+    }),
+    visible: false,
+})
+
+var pontosAcudesEstrategicos = new ol.layer.Tile({
+    title: 'Pontos Açudes Estratégicos',
+    legendUrl: url_geoserver+'?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&LAYER=prh_rpb:loc_acudes_estrategicos_rpb',
+    source: new ol.source.TileWMS({
+        url: url_geoserver,
+        params: {'LAYERS':'loc_acudes_estrategicos_rpb', 'TILED':true},
+        serverType: 'geoserver',
+    }),
+    visible: false,
+})
+
 map.addLayer(subBaciaRioParaiba);
 map.addLayer(baciaRioParaiba);
+map.addLayer(acudesMonitorados);
+map.addLayer(acudes);
 map.addLayer(hidrografiaPrincipal);
+map.addLayer(pontosAcudesMonitorados);
+map.addLayer(pontosAcudesEstrategicos);
 
 // Adicionar LayerSwitcher
 const layerSwitcher = new LayerSwitcher({
